5e3dbc0f33f40adb94018d061ce378795c11f368
Basti <klementse76573@th-nuernberg.de>
2021-03-19 21:05:24 +0100

    class und methods implementiert

--- src/main/java/ohm/softa/a02/SimpleListImpl.java
+++ src/main/java/ohm/softa/a02/SimpleListImpl.java
@@ -1,11 +1,74 @@
 package ohm.softa.a02;
 
+import java.util.Iterator;
+import java.lang.Iterable;
+
 /**
  * @author Peter Kurfer
  * Created on 10/6/17.
  */
-public class SimpleListImpl implements SimpleList {
+public class SimpleListImpl implements SimpleList, Iterable<Object> {
 
-	// TODO: Implement the required methods.
+    private static class Element {
+        private Object obj;
+        private Element next;
 
+        Element(Object obj) {
+            this.obj = obj;
+            this.next = null;
+        }
+
+        public Element GetNext() {
+            return next;
+        }
+
+        public void SetNext(Element next) {
+            this.next = next;
+        }
+    }
+
+    private Element head;
+    private int size;
+
+    public SimpleListImpl() {
+        head = null;
+    }
+
+    @Override
+    public void add(Object obj) {
+
+        if (head == null) {
+            head = new Element(obj);
+        } else {
+            Element current = head;
+            while (current.GetNext() != null) {
+                current = current.GetNext();
+            }
+            current.SetNext(new Element(obj));
+        }
+        size++;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public SimpleList filter(SimpleFilter filter) {
+
+        SimpleList list = new SimpleListImpl();
+
+        for (Object obj : this) {
+            if (filter.include(obj)) {
+                list.add(obj);
+            }
+        }
+        return list;
+    }
+
+    @Override
+    public Iterator<Object> iterator() {
+        return null;
+    }
 }
